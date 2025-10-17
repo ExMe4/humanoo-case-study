e3e17219ae1ff1db36c9df6fdf103e9934aea3ed
ExMe4 <joao.f.regateiro@gmail.com>
2025-10-17 14:44:48 +0200

    [frontend] - added lazy loading

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,24 +1,29 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BookList from "./pages/BookList";
-import BookForm from "./pages/BookForm";
 import { BookProvider } from "./context/BookContext";
 import ErrorBoundary from "./components/ErrorBoundary";
+import React, { Suspense, lazy } from "react";
+
+const BookList = lazy(() => import("./pages/BookList"));
+const BookForm = lazy(() => import("./pages/BookForm"));
 
 function App() {
   return (
-    <ErrorBoundary>
-      <BookProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<BookList />} />
-            <Route path="/books/new" element={<BookForm />} />
-            <Route path="/books/:id/edit" element={<BookForm />} />
-            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-          </Routes>
-        </Router>
-      </BookProvider>
-    </ErrorBoundary>
+    <React.StrictMode>
+      <ErrorBoundary>
+        <BookProvider>
+          <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<BookList />} />
+                <Route path="/books/new" element={<BookForm />} />
+                <Route path="/books/:id/edit" element={<BookForm />} />
+                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+              </Routes>
+            </Suspense>
+          </Router>
+        </BookProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
   );
 }
 
