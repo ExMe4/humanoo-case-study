3cd6c76d077a3d82404f326bdb6ad41a5895d6b7
ExMe4 <joao.f.regateiro@gmail.com>
2025-10-17 11:30:36 +0200

    [frontend] - react setup

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './pages/BookList';
+import BookForm from './pages/BookForm';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/books/new" element={<BookForm />} />
+        <Route path="/books/:id/edit" element={<BookForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
