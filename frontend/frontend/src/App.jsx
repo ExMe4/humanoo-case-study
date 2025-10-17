def768172c6aa792a0a48ccfdf3e9d2f7cb42b41
ExMe4 <joao.f.regateiro@gmail.com>
2025-10-17 14:03:39 +0200

    [frontend] - added error boundary component

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,19 +1,23 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookList from "./pages/BookList";
 import BookForm from "./pages/BookForm";
 import { BookProvider } from "./context/BookContext";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <BookProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<BookList />} />
-          <Route path="/books/new" element={<BookForm />} />
-          <Route path="/books/:id/edit" element={<BookForm />} />
-        </Routes>
-      </Router>
-    </BookProvider>
+    <ErrorBoundary>
+      <BookProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<BookList />} />
+            <Route path="/books/new" element={<BookForm />} />
+            <Route path="/books/:id/edit" element={<BookForm />} />
+          </Routes>
+        </Router>
+      </BookProvider>
+    </ErrorBoundary>
   );
 }
 
